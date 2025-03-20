d724410285955db931f8a15d05ecdb72e22fc5a5
Gabriel Sheeley <gabriel.sheeley@augustwenty.com>
2025-03-20 11:19:06 -0400

    Quick push

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const main = async () => {
     const repo = core.getInput('repo', { required: true });
     const pr_number = core.getInput('pr_number', { required: true });
     const token = core.getInput('token', { required: true });
+    const ref = core.getInput('ref', { required: true });
 
     /**
      * Now we need to create an instance of Octokit which will use to call
@@ -17,35 +18,37 @@ const main = async () => {
      **/
     const octokit = new github.getOctokit(token);
     // Get the list of tags sorted by date
-    
-    const { data: tags } = await octokit.repos.listTags({ owner, repo });
-    console.log("data", data);
-    let commits = [];
-    let currentTag;
-    let previousTag;
 
-    if (tags.length < 2) {
-        console.log("Not enough tags to compare.");
-        commits = this.getCommits(owner, repo, ref);
-        currentTag = tags[0].name;
-        previousTag = commits[0].name;
-    } else {
-        currentTag = tags[0].name;
-        previousTag = tags[1].name;
-    }
-
-    console.log(`Comparing commits between ${previousTag} and ${currentTag}`);
-
-    // Get the commits between the two tags
-    const { data: comparison } = await octokit.repos.compareCommits({
-        owner,
-        repo,
-        base: previousTag,
-        head: currentTag
-    });
-    console.log("commits", commits);
-
-    console.log("Commits:", comparison.commits.map(commit => commit.commit.message));
+    const branches = await this.octokit.paginate(this.octokit.rest.repos.listBranches, { owner, repo, per_page: 100 })
+    console.log("branches", branches);
+    // const { data: tags } = await octokit.repos.listTags({ owner, repo });
+    // console.log("data", data);
+    // let commits = [];
+    // let currentTag;
+    // let previousTag;
+    //
+    // if (tags.length < 2) {
+    //     console.log("Not enough tags to compare.");
+    //     commits = this.getCommits(owner, repo, ref);
+    //     currentTag = tags[0].name;
+    //     previousTag = commits[0].name;
+    // } else {
+    //     currentTag = tags[0].name;
+    //     previousTag = tags[1].name;
+    // }
+    //
+    // console.log(`Comparing commits between ${previousTag} and ${currentTag}`);
+    //
+    // // Get the commits between the two tags
+    // const { data: comparison } = await octokit.repos.compareCommits({
+    //     owner,
+    //     repo,
+    //     base: previousTag,
+    //     head: currentTag
+    // });
+    // console.log("commits", commits);
+    //
+    // console.log("Commits:", comparison.commits.map(commit => commit.commit.message));
 }
 
 async function getBranches(owner, repo) {
